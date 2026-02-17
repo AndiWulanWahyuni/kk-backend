92a885084e0e64280544ee67f83bc0b0e2d2beea
AndiWulanWahyuni <wulanandi06@gmail.com>
2026-02-18 04:33:03 +0800

    edit

--- firebase_init.py
+++ firebase_init.py
@@ -1,16 +1,32 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
-import json
 
 def init_firebase():
     if not firebase_admin._apps:
-        firebase_config = os.getenv("FIREBASE_CONFIG")
+        required_env = [
+            "FIREBASE_TYPE",
+            "FIREBASE_PROJECT_ID",
+            "FIREBASE_PRIVATE_KEY",
+            "FIREBASE_CLIENT_EMAIL",
+        ]
 
-        if not firebase_config:
-            raise ValueError("FIREBASE_CONFIG tidak ditemukan di environment variable")
+        for var in required_env:
+            if not os.getenv(var):
+                raise ValueError(f"Environment variable {var} belum di-set di Render")
 
-        cred_dict = json.loads(firebase_config)
+        cred_dict = {
+            "type": os.getenv("FIREBASE_TYPE"),
+            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+            "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+            "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+            "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+            "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+            "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+            "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER"),
+            "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT"),
+        }
 
         cred = credentials.Certificate(cred_dict)
         firebase_admin.initialize_app(cred)
